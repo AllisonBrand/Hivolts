5045b2b5ae61a8cc10ea44e44eaf539da6f8b63f
AllisonBrand <56557237+AllisonBrand@users.noreply.github.com>
2019-10-14 17:52:40 -0700

    Edit GameMaster

--- GameMaster.java
+++ GameMaster.java
@@ -1,8 +1,18 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class GameMaster {
 
 	public static void main(String[] args) {
-
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("D:\\AllisonData\\Pictures\\AP Computer Science\\Electric Fence.png	"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Failed");
+		}
 	}
 
 }
